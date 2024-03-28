f43255cb1f646e0620801c520e060d62db67871f
MFaizanShah <148917264+MFaizanShah@users.noreply.github.com>
2024-03-28 16:30:03 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def get_greeting():
-  return "Hello, World!"  # Change this message for the staging demonstration
+  return "Hello, World !"  # Change this message for the staging demonstration
 
 message = get_greeting()
 print(message)
